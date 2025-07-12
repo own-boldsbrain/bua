874abbfd2c4cf7ec6024371cf5c23def0d3b326b
Lucas Giordano <giordano3102lucas@gmail.com>
2025-07-11 19:45:47 -0700

    remove docker

--- src/bua/computers/default/__init__.py
+++ src/bua/computers/default/__init__.py
@@ -1,4 +1,3 @@
 from .browserbase import BrowserbaseBrowser
 from .local_playwright import LocalPlaywrightBrowser
-from .docker import DockerComputer
 from .scrapybara import ScrapybaraBrowser, ScrapybaraUbuntu
